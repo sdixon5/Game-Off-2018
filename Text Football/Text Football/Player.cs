9f28371548994d2e42c5bb1345882701325376eb
Shawn Dixon <shawn12dixon@gmail.com>
2018-11-24 15:43:22 -0700

    Started the Text Football Game in .Net Framework.
    Have added the game, player, and start classes.

---
+++ Text Football/Text Football/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Football
+{
+    class Player
+    {
+        public string playerName;
+        public string teamName;
+        public bool hasBall;
+
+        public void playSelection()
+        {
+
+        }
+    }
+}
